9df5a478289f4eb5f2d8845e272bea549481e9cb
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2024-10-31 08:53:15 +0200

    [Fixed] missing break

--- servicehisilicon/servicehisilicon.cpp
+++ servicehisilicon/servicehisilicon.cpp
@@ -2113,6 +2113,7 @@ RESULT eServiceHisilicon::getSubtitleList(std::vector<struct SubtitleTrack> &sub
 			{
 				track.type = 0;
 				track.page_number = 7;
+				break;
 			}
 			case HI_FORMAT_SUBTITLE_LRC:
 			case HI_FORMAT_SUBTITLE_SMI:
